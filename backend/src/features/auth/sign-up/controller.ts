b1d2f403904d6ef55bbe44be1a7a04ce1e921b4f
Sagi Barshai <sagibarshai1@gmail.com>
2024-12-24 23:29:25 +0200

    start with override unverified users that expired_in token on email_verifications table is expired

--- backend/src/features/auth/sign-up/controller.ts
+++ backend/src/features/auth/sign-up/controller.ts
@@ -5,6 +5,7 @@ import { toHash } from "../../../utils/hashes";
 import { deleteTokenCookie } from "../../../utils/jwt";
 import { sendEmailVerification } from "../../../utils/email-verification";
 import { ApiResponseJson } from "../../../types/api-response-json";
+import { SelectEmailVerificationModel } from "../models/email-verification";
 
 interface SignUpRequest extends Request {
   body: NewUserPayload;
@@ -16,10 +17,18 @@ export const signUpController = async (req: SignUpRequest, res: Response, next: 
 
     // this controller will not accept null values because it's serve only users from internal regular signup
     const hashedPassword = toHash(req.body.password!);
-    const isUserExists = await SelectUserModel(req.body.email!);
+    const user = await SelectUserModel(req.body.email!);
 
-    if (isUserExists) return next(BadRequestError([{ message: `User with email ${req.body.email} already exists`, field: "email" }]));
+    if (user) {
+      if (user.isVerified) return next(BadRequestError([{ message: `User with email ${req.body.email} already exists`, field: "email" }]));
 
+      const emailVerification = await SelectEmailVerificationModel(user.email);
+      if (emailVerification && new Date() > emailVerification?.expiredIn) {
+        // delete the user and insert new one
+        console.log("Should delete user and insert new user");
+      } else console.log("should not let override");
+      return next(BadRequestError([{ message: `User with email ${req.body.email} already exists`, field: "email" }]));
+    }
     const { safeUser } = await InsertUserModel({ ...req.body, password: hashedPassword });
 
     await sendEmailVerification({ id: safeUser.id, to: safeUser.email! });
